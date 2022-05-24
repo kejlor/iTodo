469e7b297e4d730e98c87fe9a39eab1b0230a792
kejlor <woj450@gmail.com>
2022-05-24 13:40:13 +0200

    Initial Commit

---
+++ iTodo/iTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTodoApp.swift
+//  iTodo
+//
+//  Created by Bartosz Wojtkowiak on 24/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct iTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
